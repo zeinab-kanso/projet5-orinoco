c2d8a275b986d2fb82ffaaa5fc34f9ad85363434
Zeinab Kanso <zeinab.kanso@outlook.com>
2021-08-26 09:59:37 +0200

    mettre un lien vers la page produit

--- js/index.js
+++ js/index.js
@@ -4,14 +4,16 @@ fetch('http://localhost:3000/api/teddies')
     for (let jsonProduit of jsonListProduit) {
       let produit = new Produit(jsonProduit);
       document.querySelector('.container').innerHTML += `
-        <div class="col-12 col-lg-6">
-          <div class="card mb-6 mb-lg-0 border-primary shadow">
+        <div class="row">
+          <div class="card  ">
           <img src="${produit.imageUrl}" alt="teddy" class="card-img">
           <div class="card-body">
             <h3 class="card-name"> ${produit.name} </h3>
             <p class="card-description">${produit.description}</p>
-            <span class="card-price">${produit.price} </span>
+            <span class="card-price">${produit.price / 100 + '€'} </span>
           </div>
+          <a class="btn" href="produit.html?id=${produit._id}" >
+          <span> Voir l'article </span> </a>
           </div>
         </div>`;
     }
